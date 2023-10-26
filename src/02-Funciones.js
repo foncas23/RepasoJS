26c8657cbb96af44d8aae7321ee7b778144a1f4d
Alfonso Carballal <alfonsocarballalgraupera@gmail.com>
2023-10-26 19:40:46 +0200

    Update 02-Funciones.js

--- src/02-Funciones.js
+++ src/02-Funciones.js
@@ -1,9 +1,38 @@
 //Funciones
 
-function sayHello(){
+/**
+ * Función sin parámetros
+ * @returns 
+ */
+function sayHello1(){
 
   const greeting = 'Hola mundo function!';
-  return greeting
+  return greeting;
 }
+const result = sayHello1();
+console.log(result);
 
-const result = sayHello();
+/**
+ * Función que recibe parámetro de entrada
+ * @param {*} name 
+ * @returns 
+ */
+function sayHalleo2(name){
+  const greeting = `Hola mundo function soy ${name}`;
+  return greeting;
+}
+const reuslt1 = sayHalleo2('Alfonso');
+console.log(reuslt1);
+
+/**
+ * Función que tiene valor por defecto para tener en cuenta si no se le pasa parámetro, 
+ *si le pasrámos parámetro cuando llamamos a la función sería ese el valor
+ * @param {*} name 
+ * @returns 
+ */
+function sayHalleo3(name = 'Juan'){
+  const greeting = `Hola mundo function soy ${name}`;
+  return greeting;
+}
+const reuslt3 = sayHalleo3();
+console.log(reuslt3);
