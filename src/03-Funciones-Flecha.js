03a35b394935b20f193c1a06cf003977ed9b3293
Alfonso Carballal <alfonsocarballalgraupera@gmail.com>
2023-10-26 19:57:12 +0200

    arrow function

---
+++ src/03-Funciones-Flecha.js
@@ -0,0 +1,39 @@
+//Funciones flecha
+
+/**
+ * En esta función tenemos dos parámetros de entrada, los cuales son opciones de pasar a la función
+ * cuando esta es llamada, de ser así cogerá los valores por defecto
+ * @param {*} name 
+ * @param {*} age 
+ * @returns 
+ */
+const sayHello1 = (name = 'Pepe', age = 10) =>{
+  const greeting = `Hola mundo function arrow! soy ${name} y tengo ${age} de edad`
+  return greeting
+}
+const result1 = sayHello1();
+console.log(result1)
+
+/**
+ * En esta función tenemos dos parámetros de entrada, los cuales pasamos al llamar a la función, y pisan los valores por defecto que tiene.
+ * En este ejemplo vemos que no es necesario declarar una constante para almacenar el valor y luego devolverlo.
+ * @param {*} name 
+ * @param {*} age 
+ * @returns 
+ */
+const sayHello2 = (name = 'Pepe', age = 10) =>{
+  return `Hola mundo function arrow! soy ${name} y tengo ${age} de edad`
+}
+const result2 = sayHello2('Alfonso', 30);
+console.log(result2)
+
+/**
+ * Optimización del código, es otra manera de ahorrar líneas de código, y hace exactamente lo mismo que las funciones anteriores
+ * @param {*} name 
+ * @param {*} age 
+ * @returns 
+ */
+const sayHello3 = (name = 'Pepe', age = 10) =>  `Hola mundo function arrow! soy ${name} y tengo ${age} de edad`
+
+const result3 = sayHello3('Alfonso', 30);
+console.log(result3)
